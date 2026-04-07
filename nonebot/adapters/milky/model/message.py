f09d01d97e117473c076e2e59599c2b2dffa9ef3
RF-Tar-Railt <rf_tar_railt@qq.com>
2026-04-07 11:43:10 +0800

    :sparkles: update Models

--- nonebot/adapters/milky/model/message.py
+++ nonebot/adapters/milky/model/message.py
@@ -51,6 +51,8 @@ class IncomingMessage(ModelBase):
 
 class IncomingForwardedMessage(ModelBase):
     """接收的转发消息"""
+    message_seq: int | None = None
+    """消息序列号"""
 
     sender_name: str
     """发送者名称"""
